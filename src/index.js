9d294e28b2375add4b2d2f23fe04a62dd5e7356c
veronique.savard <veronique.savard@verionmedia.com>
2020-03-06 11:33:47 +0000

    can pass javascript objects

--- src/index.js
+++ src/index.js
@@ -6,14 +6,19 @@ import './index.css';
 // function getButtonText() {
 //     return 'Click on Me!';
 // }
+
+// Objects are not valid as a React child
 const Blue = ()=> {
-    const buttonText = "Please Click Here!";
+    // const buttonText = "Please Click Here!";
+    // Not allowed to print out unless refering to key
+    const buttonText = {text: "Click Me"};
+    const style = {backgroundColor:'blue', color:'white'};
     return (<div>
 
          <label className="label" for="name"> Enter name: </label>
         <input id="name" type="text" />
         {/*<button style={{backgroundColor:'blue', color:'white'}}>{getButtonText()}</button>*/}
-        <button style={{backgroundColor:'blue', color:'white'}}>{buttonText}</button>
+        <button style={style}>{buttonText.text}</button>
     </div>)
 }
 ReactDOM.render(
