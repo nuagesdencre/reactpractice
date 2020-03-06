d66e0ece935812c99d4bab23482bee5a7e928bc6
veronique.savard <veronique.savard@verionmedia.com>
2020-03-06 11:29:34 +0000

    passing javascript function or reference

--- src/index.js
+++ src/index.js
@@ -3,13 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+// function getButtonText() {
+//     return 'Click on Me!';
+// }
 const Blue = ()=> {
+    const buttonText = "Please Click Here!";
     return (<div>
-        <button style={{backgroundColor:'blue', color:'white'}}>Test Blue Button</button>
+
+         <label className="label" for="name"> Enter name: </label>
+        <input id="name" type="text" />
+        {/*<button style={{backgroundColor:'blue', color:'white'}}>{getButtonText()}</button>*/}
+        <button style={{backgroundColor:'blue', color:'white'}}>{buttonText}</button>
     </div>)
 }
 ReactDOM.render(
-    <App />,
-   // <Blue />,
+    // <App />,
+   <Blue />,
   document.getElementById('root')
 );
