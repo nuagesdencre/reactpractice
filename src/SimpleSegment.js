8e24191a05f610a1cafd8bbe52ca5ec7af55a5ba
veronique.savard <veronique.savard@verionmedia.com>
2020-03-06 16:49:52 +0000

    creation of a simple segment accepting children through props

---
+++ src/SimpleSegment.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SimpleSegment = (props) => {
+    return <div className="ui placeholder segment"> {props.children} </div>
+
+};
+
+export default SimpleSegment;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from "./CommentDetail";
 import ApprovalCard from "./ApprovalCard";
+
+import SimpleSegment from "./SimpleSegment";
 import faker from "faker";
 
 const App = () => {
     return (
+
         <div className="ui container comments">
-            <ApprovalCard> <CommentDetail author="Sam" timePosted="Today" photo ={faker.image.avatar()} content = "This is fun."/></ApprovalCard>
-            <ApprovalCard><CommentDetail author="Ophelie" timePosted="Today" photo ={faker.image.avatar()} content = "Cannot believe it!"/></ApprovalCard>
-                <ApprovalCard><CommentDetail author="John" timePosted="Yesterday" photo ={faker.image.avatar()} content = "No way."/></ApprovalCard>
+            <SimpleSegment><ApprovalCard>
+                <h3>Warning!</h3>
+                Are you sure you want to go forward?</ApprovalCard>
+            </SimpleSegment>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timePosted="Today" photo={faker.image.avatar()}
+                               content="This is fun."/></ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Ophelie" timePosted="Today" photo={faker.image.avatar()}
+                               content="Cannot believe it!"/></ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="John" timePosted="Yesterday" photo={faker.image.avatar()}
+                               content="No way."/></ApprovalCard>
         </div>
+
     )
 };
 
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
