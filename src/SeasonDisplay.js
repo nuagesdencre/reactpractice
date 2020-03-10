8d74ae339b492b2d8e6f0adf9611ec537fd84dc3
veronique.savard <veronique.savard@verionmedia.com>
2020-03-10 17:09:26 +0000

    checking season based on current month and hemisphere

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-    return <div> Season </div>
+const getSeason = (lat, month) => {
+    if (lat < 0) {
+        if (month < 3 && month > 8) {
+            return <h2>Winter</h2>
+        }
+        return <h2>Summer</h2>
+
+    }
+    if (lat >= 0) {
+        if (month < 3 && month > 8) {
+            return <h2>Summer</h2>
+        }
+        return <h2>Winter</h2>
+
+    }
+}
+
+const SeasonDisplay = (props) => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    return (
+        <div>
+            <h2>Lat: {props.lat}</h2>
+            {season} </div>
+    )
 }
 
 export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,35 @@
 // SEASON project practice
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {lat: null, errMsg: ''};
+    state = {lat: null, errMsg: ''};
+
+
+    componentDidMount() {
+        //calls at the time of mounting, only once
+        //all the data-loading code should be here
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({lat: position.coords.latitude}),
+                err => this.setState({errMsg: err.message}))
+
+        console.log('My component rendered to the screen!')
     }
 
-componentDidMount(){
-   //calls at the time of mounting, only once
-    //all the data-loading code should be here
-    window.navigator.geolocation.getCurrentPosition(
-        position => this.setState({lat: position.coords.latitude}), err => this.setState({errMsg: err.message});
-    console.log('My component rendered to the screen!')
-}
-    componentDidUpdate(){
-   //calls at the time of updating, anytime there is an update of the component
+    componentDidUpdate() {
+        //calls at the time of updating, anytime there is an update of the component
         //possible to add data-loading code that is repeated (multiple calls to the network, for instance)
         console.log('My component was just updated -- it rerendered!')
-}
-
-// componentDidUnmount(){
-//         //to remove the component from the screen (cleanup)
-// }
+    }
 
     render() {
 
         if (this.state.errMsg && !this.state.lat) {
             return <h3> Error: {this.state.errMsg}</h3>
         } else if (!this.state.errMsg && this.state.lat) {
-            return <h1> Latitude: {this.state.lat} </h1>
+            return <SeasonDisplay lat={this.state.lat} />
         } else {
             return <h1> Loading! </h1>
         }
