d60a83fc84f7199276d0de5fdfbbc8dca0e8f1d5
veronique.savard <veronique.savard@verionmedia.com>
2020-03-10 10:32:31 +0000

    working on class components

--- src/SimpleSegment.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const SimpleSegment = (props) => {
-    return <div className="ui placeholder segment"> {props.children} </div>
-
-};
-
-export default SimpleSegment;
--- src/index.js
+++ src/index.js
@@ -1,35 +1,12 @@
+// SEASON project practice
 import React from 'react';
 import ReactDOM from 'react-dom';
-import CommentDetail from "./CommentDetail";
-import ApprovalCard from "./ApprovalCard";
-
-import SimpleSegment from "./SimpleSegment";
-import faker from "faker";
 
 const App = () => {
-    return (
-
-        <div className="ui container comments">
-            <SimpleSegment><ApprovalCard>
-                <h3>Warning!</h3>
-                Are you sure you want to go forward?</ApprovalCard>
-            </SimpleSegment>
-            <ApprovalCard>
-                <CommentDetail author="Sam" timePosted="Today" photo={faker.image.avatar()}
-                               content="This is fun."/></ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author="Ophelie" timePosted="Today" photo={faker.image.avatar()}
-                               content="Cannot believe it!"/></ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail author="John" timePosted="Yesterday" photo={faker.image.avatar()}
-                               content="No way."/></ApprovalCard>
-        </div>
-
-    )
-};
-
+    return <div> HI THERE!</div>
+}
 
 ReactDOM.render(
     <App/>,
-    document.getElementById('root')
-);
+    document.querySelector('#root')
+)
