f071bcd8f870bac7e0b1a9bf29c538c49f0ee1e7
veronique.savard <veronique.savard@verionmedia.com>
2020-03-10 22:15:12 +0000

    practice redux

--- src/ReduxPractice.js
+++ src/ReduxPractice.js
@@ -29,7 +29,7 @@ const createClaim =(name, amountOfMoney) => {
 
 //reducers
 const claimsHistory = (oldListOfClaims=[], action) =>{
-    if(action.type === CREATE_CLAIM){
+    if(action.type === 'CREATE_CLAIM'){
         //we care about the action
         return [...oldListOfClaims, action.payload]
     }
@@ -37,25 +37,41 @@ const claimsHistory = (oldListOfClaims=[], action) =>{
     return oldListOfClaims;
 }
 
-const accounting = (oldBagOfMoney=100, action) =>{
-    if(action.type === CREATE_CLAIM){
+const policies = (oldPolicies=[], action) =>{
+    if(action.type === 'CREATE_POLICY'){
+        //we care about the action
+        return [...oldPolicies, action.payload.name]
+    } else if (action.type === 'DELETE_POLICY'){
+        return oldPolicies.filter(i => i !== action.payload.name)
+    }
+    //we don't care about the action
+    return oldPolicies;
+}
+
+const accounting = ( oldBagOfMoney=100, action) =>{
+    if(action.type === 'CREATE_CLAIM'){
         //we care about the action
         return oldBagOfMoney- action.payload.amountOfMoney
-    } else if (action.type === CREATE_POLICY){
+    } else if (action.type === 'CREATE_POLICY'){
         return oldBagOfMoney + action.payload.membership
     }
     //we don't care about the action
     return oldBagOfMoney;
 }
 
-const policies = (oldPolicies=[], action) =>{
-    if(action.type === CREATE_POLICY){
-        //we care about the action
-        return [...oldPolicies, action.payload.name]
-    } else if (action.type === DELETE_POLICY){
-        return oldPolicies.filter(i => i === action.payload.name)
-    }
-    //we don't care about the action
-    return oldPolicies;
-}
+const {createStore, combineReducers} = Redux;
+const ourDepartments = combineReducers({
+    accounting: accounting,
+    claimsHistory: claimsHistory,
+    policies: policies
+})
+const store = createStore(ourDepartments)
 
+const action = createPolicy('Lucy', 20);
+
+store.dispatch(action);
+store.dispatch(createPolicy('Emilie', 20));
+store.dispatch(createPolicy('Sam', 40));
+store.dispatch(createClaim('Sam', 80));
+store.dispatch(deletePolicy('Emilie'));
+console.log(store.getState());
