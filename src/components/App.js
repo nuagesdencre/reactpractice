33f7cef08d76ae227c3211eeba9f99f22382342b
veronique.savard <veronique.savard@verionmedia.com>
2020-03-16 10:36:03 +0000

    practice ongoing...

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from "./SearchBar";
 
-const App= ()=> {
-return (
-    <div className="ui container" style={{marginTop:'10px'}}>
-    <SearchBar/>
-</div>)
+const API_KEY = process.env.REACT_APP_PICS_API_KEY ;
+
+class App extends React.Component {
+
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: {
+                query: term
+            },
+            headers: {
+                Authorization: API_KEY
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>)
+    }
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,22 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = {term: ''};
+
+    onFormSubmit = event => {
+        event.preventDefault();
+
+        this.props.onSubmit(this.state.term)
+    }
 
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" onChange={(event)=> console.log(event.target.value)} placeholder="Search..."/>
+                        <input type="text" value={this.state.term}
+                               onChange={event => this.setState({term: event.target.value})} placeholder="Search..."/>
                     </div>
                 </form>
             </div>
